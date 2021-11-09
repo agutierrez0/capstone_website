0eac3dc3c0860a65e0aa7f52ad9166ba1436f8c1
Angel Gutierrez <agutierrez01@manhattan.edu>
2021-11-09 10:17:07 -0500

    starting to add signup modal

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './styles.css';
 import Navbar from './modules/Navbar'
 
 const serviceName = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'
 const characteristicName = 'beb5483e-36e1-4688-b7f5-ea07361b26a8'
+
 export default function Dashboard() {
+
+    useEffect(() => {
+        var modal = document.getElementById('myModal');
+        var container = document.getElementById('myContainer');
+        if (modal && container) {
+            modal.className = "Modal is-visuallyHidden";
+            setTimeout(function() {
+                container.className = "MainContainer is-blurred";
+                modal.className = "Modal";
+            }, 100);
+            container.parentElement.className = "ModalOpen";
+        }
+    }, [])
+
     function getBluetoothDevice() {
-        let config = { filters: [{
-            name: 'Long name works now',
-            
-          }],
-          optionalServices: [serviceName]}
+        let config = { filters: [{ name: 'Long name works now' }], optionalServices: [serviceName]}
         navigator.bluetooth.requestDevice(config)
         .then(device => {
             console.log(device)
@@ -36,13 +47,20 @@ export default function Dashboard() {
     }
 
     return <div>
-            <Navbar />
-            <div className="dash-container">
-                <h2> Welcome to RC Automapper </h2>
+                <Navbar />
+                <div id="myContainer" className="dash-container">
+                    <h2> Welcome to RC Automapper </h2>
 
-                <div className="button-section-dash">
-                    <button onClick={getBluetoothDevice}>Connect RC Car</button>
+                    <div id="myModal" class="Modal is-hidden is-visuallyHidden">
+                        <div class="Modal-content">
+                            <span id="closeModal" class="Close">&times;</span>
+                            <p>Simple Modal</p>
+                        </div>
+                    </div>
+
+                    <div className="button-section-dash">
+                        <button onClick={getBluetoothDevice}>Connect RC Car</button>
+                    </div>
                 </div>
             </div>
-        </div>
 }
