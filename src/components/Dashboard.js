d952840fae3c73edbb1a5758f2b44a908e5399ee
Angel Gutierrez <agutierrez01@manhattan.edu>
2021-12-08 11:23:26 -0500

    dynamic stop/start reading

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,34 @@
 import React, { useState } from 'react'
-import './styles.css';
 import Navbar from './modules/Navbar'
+import './styles.css';
 
 const serviceName = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'
+const uuids = ['beb5483e-36e1-4688-b7f5-ea07361b26a8', '5d0faa6a-5086-11ec-bf63-0242ac130002', '8fa2e117-e431-4dab-ab5a-24ba2067983d']
 
-const uuids = ['beb5483e-36e1-4688-b7f5-ea07361b26a8', '5d0faa6a-5086-11ec-bf63-0242ac130002']
-/*
-const uuidToWord = {
-    'beb5483e-36e1-4688-b7f5-ea07361b26a8' : 'topic',
-    '5d0faa6a-5086-11ec-bf63-0242ac130002' : 'data'
-}
-*/
 export default function Dashboard() {
     const [myDevice, setMyDevice] = useState(null)
     const [myServer, setMyServer] = useState(false)
+    const [switchToggle, setSwitchToggle] = useState(false)
+    const [myInterval, setMyInterval] = useState(null)
 
-    function readCharacteristicValue(uuid) {
+    function handleToggleSwitch() { 
+        if (!switchToggle) {
+            console.log('start reading characteristics')
+            setMyInterval(setInterval(() => {
+                readCharacteristicValue(uuids[0], 'ultrasonic-sensor')
+                readCharacteristicValue(uuids[1], 'movement-status')
+                readCharacteristicValue(uuids[2], 'manometer-reading')
+            }, 1000))
+        } else {            
+            console.log('stop reading characteristics')
+            console.log(myInterval)
+            clearInterval(myInterval)
+            setMyInterval(null)
+        }
+        setSwitchToggle(!switchToggle)
+    }
+
+    function readCharacteristicValue(uuid, elementId) {
         if (myServer) {
             myServer.getPrimaryService(serviceName)
             .then(service => service.getCharacteristic(uuid))
@@ -24,17 +37,13 @@ export default function Dashboard() {
                 if (value) {
                     var decoder = new TextDecoder()
                     const message = decoder.decode(value)
-                
+                    const thisElement = document.getElementById(elementId)
                     if (message.length === 4) {
                         console.log(`message isn't string: ${value.getUint8(0)}`)
-                        const dataElement = document.getElementById('data-value')
-                        dataElement.innerText = `data: ${value.getUint8(0)} cm`
-
+                        thisElement.innerText = `${value.getUint8(0)} cm`
                     } else {
                         console.log(`message is string: ${message}`)
-                        const topicElement = document.getElementById('topic-value')
-                        topicElement.innerText = `topic: ${message}`
-                        return message
+                        thisElement.innerText = message
                     }
                 } else {
                     alert('error reading value')
@@ -46,39 +55,16 @@ export default function Dashboard() {
         }
     }
 
-    function getCharacteristics() {
-        myServer.getPrimaryService(serviceName)
-        .then(service => {
-            console.log('service accessed')
-            return service.getCharacteristics();
-        })
-        .then(_ => {
-            var count = 0;
-            var myInterval = setInterval(() => {
-                readCharacteristicValue(uuids[0])
-                readCharacteristicValue(uuids[1])
-                if (count === 100) { clearInterval(myInterval) }
-                count = count + 1;
-            }, 1000)
-        })
-        .catch(error => { return console.error(error); });
-        return;
-    }
-
     function connectToGattServer() {
-        console.log('here')
         if (myDevice) {
-            console.log('here1')
             myDevice.gatt.connect()
             .then(server => {
-                console.log('here2')
                 if (server) {
                     setMyServer(server)
                 }
                 return;
             })
             .catch(error => { return console.error(error); });
-            console.log('here3')
             return;
         } else {
             console.log('here4')
@@ -103,14 +89,59 @@ export default function Dashboard() {
                 <Navbar />
                 <div id="myContainer" className="dash-container">
                     <h2> Welcome to RC Automapper </h2>
-                    {myDevice ? <div>{myDevice.name} is connected <button onClick={connectToGattServer}>connect to gatt server</button> </div> : <button onClick={getBluetoothDevice}>connect device</button>}
-                    {myServer && myDevice ? <div>
-                        <div>connected to gatt server </div>
-                        <button onClick={getCharacteristics}>read characteristics</button>
-                        <div id="topic-value"></div>
-                        <div id="data-value"></div>
-                    </div> : null}
 
+                    <div>
+                    {myDevice ? 
+                        <div className="connect-device-section">
+                            <div className="connected-section"><h4>"{myDevice.name}" connected</h4>&nbsp;✅</div>
+                            {myServer ? 
+                            <div className="connect-device-section">
+                                <div className="connected-section"><h4>GATT Server connected</h4>&nbsp;✅</div>
+
+                                <h4>Start / Stop reading characteristics</h4>
+                                
+                                <label className="switch">
+                                    <input onClick={handleToggleSwitch} type="checkbox" />
+                                    <span className="slider round"></span>
+                                </label> 
+
+                                {switchToggle ? <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Value</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Ultrasonic Sensor Reading</td>
+                                            <td id="ultrasonic-sensor"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Movement Status</td>
+                                            <td id="movement-status"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Manometer Reading</td>
+                                            <td id="manometer-reading"></td>
+                                        </tr>
+                                    </tbody>
+                                </table> : null}
+                                <div id="topic-value"></div>
+                                <div id="data-value"></div>
+                            </div> 
+                            : 
+                            <div className="connect-device-section">
+                                <h4>Please connect to the GATT server</h4>
+                                <button onClick={connectToGattServer}>Connect to GATT server</button>
+                            </div>}
+                        </div> 
+                        :
+                        <div className="connect-device-section">
+                            <h4>Please connect the device.</h4>
+                            <button onClick={getBluetoothDevice}>Search for bluetooth device</button>
+                        </div>}
+                    </div>
                 </div>
             </div>
 }
