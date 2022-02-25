60434ee34538ff05b1b97022715314046a6ef0ea
Angel Gutierrez <agutierrez01@manhattan.edu>
2022-02-24 19:08:20 -0500

    adding route pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
 import Data from './components/Data';
+import Settings from './components/Settings';
+import About from './components/About';
 import  { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/modules/Navbar';
 
 export default function App() {
   return <div>
-    <Navbar />
-        <BrowserRouter>
-
-          <Routes>
-            <Route index element={<Dashboard />} />
-            <Route path="/data" element={<Data />} />
-            <Route path="*" element={<div>Not Found</div>} />
-          </Routes>
-        </BrowserRouter>
-          
+          <Navbar />
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Dashboard />} />
+              <Route path="/data" element={<Data />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<div>Not Found</div>} />
+            </Routes>
+          </BrowserRouter>
         </div>
 }
