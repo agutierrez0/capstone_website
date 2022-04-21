989cc1d05a0ad615ead1995268c69b81463ee13a
Angel Gutierrez <agutierrez01@manhattan.edu>
2022-04-21 18:53:27 -0400

    adding login functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import Dashboard from './components/altDashboard';
-//import Data from './components/Data';
-//import Settings from './components/Settings';
-import About from './components/About';
 
-import  { Routes, Route } from 'react-router-dom';
 import Register from './components/register';
 import Landing from './components/landing';
+import  { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return <Routes>
     <Route />
               <Route index element={<Landing />} />
               <Route path="/register" element={<Register />} />
-              <Route path="/about" element={<About />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="*" element={<div>Not Found</div>} />
         </Routes>
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,25 +1,49 @@
 import React, { useState } from 'react';
 import { Button, LoadingOverlay, TextInput } from '@mantine/core';
 import { useNotifications } from "@mantine/notifications";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from "firebase/firestore"; 
+import { firebaseConfig } from './firebaseConfig';
 import './css/landing.css';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default function Register({navigate}) {
     const [visible, setVisible] = useState(false);
-    const [inputDisable, setInputDisabled] = useState(false)
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [isValidUser, setIsValidUser] = useState(null);
     const notifications = useNotifications();
 
     async function handleLogin() {
-        if (username == "" || password == "") {
+        var userFound = false
+        setVisible(true)
+        if (username === "" || password === "") {
             notifications.showNotification({
                 title: 'Invalid inputs.',
                 message: 'Full Name / Title cannot be empty.',
                 color: 'red'
             })
         } else {
-            setVisible(true)
+            const querySnapshot = await getDocs(collection(db, "capstone_users"))
+            querySnapshot.forEach((item) => {
+                const actualItem = item.data();
+
+                if (actualItem.username === username && actualItem.password === password) {
+                    userFound = true
+                    setIsValidUser(true)
+                }
+            })
+        }
+
+        if (userFound) {
+            setTimeout(() => {
+                window.location.href = "/dashboard"
+            }, 1500)
+        } else {
+            alert('invalid username/password')
+            setVisible(false)
         }
     }
 
@@ -35,7 +59,7 @@ export default function Register({navigate}) {
                             <h6>Username</h6>
                             <TextInput 
                                 onChange={(event) => setUsername(event.target.value)}
-                                disabled={inputDisable}
+                                disabled={visible}
                                 value={username}
                                 style={{fontFamily:'RobotoSerif'}}
                                 required={true} />
@@ -45,7 +69,7 @@ export default function Register({navigate}) {
                             <h6>Password</h6>
                             <TextInput 
                                 onChange={(event) => setPassword(event.target.value)}
-                                disabled={inputDisable}
+                                disabled={visible}
                                 value={password}
                                 style={{fontFamily:'RobotoSerif'}}
                                 required />
@@ -53,7 +77,7 @@ export default function Register({navigate}) {
 
                         <Button 
                             style={{marginTop: '5%', marginBottom: '5%', fontSize: '1vw', fontFamily: 'RobotoSerif'}}
-                            disabled={inputDisable}
+                            disabled={visible}
                             onClick={handleLogin}>Log in</Button>
                             <h6 style={{textAlign: 'center', cursor: 'pointer', textDecoration: 'underline'}} onClick={() => window.location.href = "/register"}>Click here to register</h6>
                         {isValidUser === true ? <h5 style={{marginTop: "2%", color: 'green', textAlign: 'center'}}>Welcome!</h5> : null}
