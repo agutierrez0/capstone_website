9a9e5717606fa40fff6874e155a15c58af4ac4d4
Angel Gutierrez <agutierrez01@manhattan.edu>
2022-04-24 14:57:10 -0400

    spacing, tabs, etc

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import  { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return <Routes>
-    <Route />
-              <Route index element={<Landing />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="*" element={<div>Not Found</div>} />
-        </Routes>
+      <Route index element={<Landing />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<div>Not Found</div>} />
+    </Routes>
 }
